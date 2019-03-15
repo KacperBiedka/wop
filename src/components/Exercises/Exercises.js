d91fd07769c60e528d638526a3fd72d60108d1cc
Kacper <kacper.biedka.00@gmail.com>
2019-03-15 18:40:18 +0100

    Made the Exercises component work after refresh by passing redux data to local storage

--- src/components/Exercises/Exercises.js
+++ src/components/Exercises/Exercises.js
@@ -2,15 +2,42 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import ExerciseCard from './ExerciseCard/ExerciseCard';
+import NavBar from '../Navbar/Navbar';
 
 class Exercises extends Component {
+    state = {
+        exercises: []
+    }
+
+    componentWillMount() {
+        console.log(this.props.exercises);
+        console.log(this.state.exercises);
+        if (this.props.exercises) {
+           window.localStorage.clear();
+           window.localStorage.setItem('exercises', JSON.stringify(this.props.exercises));
+           console.log(window.localStorage.getItem('exercises'));
+           this.setState({
+               exercises: this.props.exercises
+           })
+        } else {
+            let localStorageExercises = JSON.parse(window.localStorage.getItem('exercises'));
+            console.log(localStorageExercises);
+            this.setState({
+                exercises: localStorageExercises
+            });
+        }
+        console.log(this.state.exercises);   
+    }
+  
+
     render() {
         return (
         <div>
+            <NavBar/>
             <h1>Exercises Loaded</h1>
             {console.log(this.props.exercises)}
-            {
-                this.props.exercises.map(ex => {
+            {  
+                this.state.exercises.map(ex => {
                     return (
                         <ExerciseCard
                         key={ex.exercise.exerciseName + ex.exercise.exerciseNumber} 
