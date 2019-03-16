322b08932b7987c657b17c1eb01ede6baa67a26f
Kacper <kacper.biedka.00@gmail.com>
2019-03-17 00:35:06 +0100

    Added the remove workout function and implemented it to the workouts menu, also fixed some key bugs

--- src/components/Workouts/Workouts.js
+++ src/components/Workouts/Workouts.js
@@ -15,7 +15,8 @@ class Workouts extends Component {
     exercises: [],
     workoutCards: {},
     loading: true,
-    reduxExercises: []
+    reduxExercises: [],
+    removedDocId: null
   }
 
   componentDidMount() {
@@ -62,6 +63,66 @@ class Workouts extends Component {
       console.log(this.state.exercises);
     }
 
+    removeWorkout = (number) => {
+    console.log("removeWorkout function triggered " + number);
+    const db = firebase.firestore();  
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        console.log(user.uid);
+        const uid = user.uid;
+        const deleteQuery = db.collection("workouts").where("uid", "==", uid).where("workoutNumber", "==", number).limit(1);
+        const subtractNumberQuery = db.collection("workouts").where("uid", "==", uid);
+        deleteQuery.get()
+        .then((snapshot) => {
+          snapshot.docs.forEach(doc => {
+          firebase.firestore().collection("workouts").doc(doc.id).delete()
+          .then(function() {
+            subtractNumberQuery.get()
+            .then((snapshot) => {
+              snapshot.docs.forEach(doc => {
+                console.log(doc.data().workoutNumber);
+                if (doc.data().workoutNumber > number) {
+                db.collection("workouts").doc(doc.id).update({
+                  workoutNumber: doc.data().workoutNumber-1
+                })
+                .then(() => {
+                  db.collection("users").doc(uid).get()
+                  .then((doc) => {
+                    console.log(doc.data().workoutsNumber);
+                    db.collection("users").doc(uid).update({
+                      workoutsNumber: doc.data().workoutsNumber - 1
+                    })
+                    .then(() => {
+                      console.log("successfuly changed the user workoutsNumber")
+                      window.location.reload();
+                    })
+                  })
+                  .catch((error) => {
+                    console.log("error changing user workoutsNumber : ", error);
+                  })
+                })
+              }
+            })
+            })
+            .catch((error) => {
+              console.log("error subtracting workoutNumbers: ", error);
+            })
+            console.log("successfully removed the workout");
+          })
+          .catch(function(error) {
+            console.error("Error removing document: ", error);
+          });
+        });
+      })
+      .catch(function(error) {
+        console.log("Error getting removedWorkout Doc ID: " + error);
+      })
+      } else {
+          console.log("Error getting document");
+      }
+    });
+    }
+
   render() {
         return (
           <div className={classes.workoutsDiv}>
@@ -70,14 +131,14 @@ class Workouts extends Component {
               {
                 this.state.workouts.map(w => {
                   return (
-                    <div className={classes.workoutCardDiv + " shadow p-3 mb-1 bg-white rounded"}>
+                    <div key={w.number + w.name} className={classes.workoutCardDiv + " shadow p-3 mb-1 bg-white rounded"}>
                     <div className={classes.workoutCardHeaderDiv}>
                     <h5 onClick={this.logData} className={classes.workoutNameHeader}>{w.name}</h5>
                     </div>
                     <ul className={classes.workoutCardList}>
                   { 
                     this.state.exercises[w.number-1].map(ex => {
-                    return <li className={classes.workoutCardListItem}>{ex.exercise.exerciseName} {ex.exercise.sets} sets {ex.exercise.reps} reps with {ex.exercise.weight}kg</li>
+                    return <li key={ex.exercise.exerciseName + ex.exercise.exerciseNumber} className={classes.workoutCardListItem}>{ex.exercise.exerciseName} {ex.exercise.sets} sets {ex.exercise.reps} reps with {ex.exercise.weight}kg</li>
                     })
                   }
                   </ul>
@@ -87,6 +148,9 @@ class Workouts extends Component {
                     <p onClick={() => this.props.getExercisesToRedux(this.state.exercises[w.number-1])} className={classes.modalAnchor}>choose</p>
                   </div>
                   </Link>
+                  <div className={classes.deleteDiv}>
+                    <a className={classes.modalAnchor + " " + classes.deleteAnchor} onClick={() => this.removeWorkout(w.number)}>delete</a>
+                  </div>
                   </div>
                 )
                 })
