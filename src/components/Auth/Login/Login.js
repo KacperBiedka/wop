bb941a780268c2ce3487a6a62122bf60513e4cb8
Kacper <kacper.biedka.00@gmail.com>
2019-04-11 23:17:12 +0200

    Changed the whole login process, not finished yet

--- src/components/Auth/Login/Login.js
+++ src/components/Auth/Login/Login.js
@@ -1,81 +1,125 @@
 import React, { Component } from 'react';
 import firebase from '../../../firebase.js';
 import * as firebaseui from 'firebaseui';
-import classes from './Login.module.css';
-import { Link } from 'react-router-dom';
+import classes from './Login.module.sass';
 
 class Login extends Component {
-  state = {
+state = {
     email: '',
     password: '',
     loginError: null
+}
+
+componentDidMount = () => {
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      window.location.href = '/workouts';
+    } else {
+      console.log("User not logged in");
+    }
+  });
+}
+
+
+render() {
+  
+  const updateEmailState = (e) => {
+    this.setState({
+      email: e.target.value
+    })
+  }
+
+  const updatePasswordState = (e) => {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+  const loginWithEmailAndPassword = () => {
+    firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
+    .then(function() {
+      window.location.href = '/workouts';
+    })
+    .catch(error => {
+      var errorComponent = null;
+      var errorMessage = error.message;
+      console.log(errorMessage);
+      errorComponent = <div className={classes.loginErrorDiv}>{error.message}</div>
+      this.setState({
+        loginError: errorComponent
+      })
+    })
+  }
+
+  const loginWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      var token = result.credential.accessToken;
+      var user = result.user;
+      console.log("login info: ", token, user);
+      window.location.href = '/workouts';
+    }).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      var email = error.email;
+      var credential = error.credential;
+      alert("Error logging in: " + error.message);
+    });
+  }
+
+  const loginWithFacebook = () => {
+    const provider =  new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      var token = result.credential.accessToken;
+      var user = result.user;
+      console.log("login info: ", token, user);
+      window.location.href = '/workouts';
+    }).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      var email = error.email;
+      var credential = error.credential;
+      alert("Error logging in: " + error.message);
+    });
+  }
+
+  const loginWithTwitter = () => {
+    const provider =  new firebase.auth.TwitterAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      var token = result.credential.accessToken;
+      var user = result.user;
+      console.log("login info: ", token, user);
+      window.location.href = '/workouts';
+    }).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      var email = error.email;
+      var credential = error.credential;
+      alert("There is no twitter user signed in");
+    });
   }
 
 
-    render() {
-        var uiConfig = {
-            signInSuccessUrl: 'workouts',
-            signInOptions: [
-              firebase.auth.GoogleAuthProvider.PROVIDER_ID
-            ],
-            tosUrl: 'privacy',
-            privacyPolicyUrl: function() {
-              window.location.assign('<your-privacy-policy-url>');
-            }
-          };
-          const updateEmailState = (e) => {
-            this.setState({
-              email: e.target.value
-            })
-          }
-
-          const updatePasswordState = (e) => {
-            this.setState({
-              password: e.target.value
-            })
-          }
-
-          const loginWithEmailAndPassword = () => {
-            firebase.auth().signInWithEmailAndPassword(this.state.email, this.state.password)
-            .then(function() {
-              window.location.href = '/workouts';
-            })
-            .catch(error => {
-              var errorComponent = null;
-              var errorMessage = error.message;
-              console.log(errorMessage);
-              errorComponent = <div className={classes.loginErrorDiv}>{error.message}</div>
-              this.setState({
-                loginError: errorComponent
-              })
-            })
-          }
-
-          let ui = firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(firebase.auth());
-          ui.start('#firebaseui-auth-container', uiConfig);
-
-
-
        return (
-          <div className={"shadow p-3 mb-5 bg-white rounded " + classes.mainDiv}>
-            <h2 className={classes.loginHeader}>Login</h2>
-            <div className={classes.emailAuthContainer}>
-            <input value={this.state.email} onChange={updateEmailState} className={classes.inputField} type="email" placeholder="email" />       
-            <input value={this.state.password} onChange={updatePasswordState} className={classes.inputField + " " + classes.passwordInput} type="password" placeholder="password" />
-            <button onClick={loginWithEmailAndPassword}className={classes.authButton + " " + classes.loginButton}>
-              Login
-            </button>
-            <Link to='/signup'>
-              <button className={classes.authButton + " " + classes.signupButton}>
-                Sign up
-              </button>
-            </Link>
+        <div className={classes.mainDiv + " shadow p-3 mb-1 bg-white rounded"}>
+          <div className={classes.authDiv + " row"}>
+            <h1 className={classes.mainHeader}>Login</h1>
+            <div className={classes.standardloginDiv + " col-6"}>
+              <p className={classes.inputParagraph}>Email</p>
+              <input className={classes.inputField}  onChange={updateEmailState} type="email" placeholder=""/>
+              <p className={classes.inputParagraph}>Password</p>
+              <input className={classes.inputField} onChange={updatePasswordState} type="password" placeholder=""/>
+              <button className={classes.loginButton} onClick={loginWithEmailAndPassword}>LOGIN</button>
+              <p className={classes.forgotPassword}>Forgot password ?</p>
             </div>
-            <p className={classes.dividingParagraph}>or</p>
-            <div id="firebaseui-auth-container" className={classes.firebaseuiDiv}>
+            <div className="col-6">
+              <button className={classes.googleAuthButton} onClick={loginWithGoogle}><i className={classes.buttonIcon + " fab fa-google"}></i>Login with Google</button>
+              <button className={classes.twitterAuthButton} onClick={loginWithTwitter}><i className={classes.buttonIcon + " fab fa-twitter"}></i>Login with Twitter</button>
+              <button className={classes.facebookAuthButton} onClick={loginWithFacebook}><i className={classes.buttonIcon + " fab fa-facebook"}></i>Login with Facebook</button>  
             </div>
-          {this.state.loginError}
           </div>
+          {this.state.loginError}
+         </div>
         )
     }
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,24 +1,50 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 import firebase from '../../firebase';
 import classes from './Home.module.sass';
 
 class Home extends Component {
+state = {
+  class: "",
+  displayedContent: null
+}
+
 render() {
-return (
-<div className={classes.mainDiv + " shadow p-3 mb-1 bg-white rounded"}>
-    <div className={classes.authDiv}>
-     <h1 className={classes.mainHeader}>Workout Planner</h1>
-     <p className={classes.inputParagraph}>Email</p>
-     <input className={classes.inputField} type="email" placeholder=""/>
-     <p className={classes.inputParagraph}>Password</p>
-     <input className={classes.inputField} type="password" placeholder=""/>
-     <p className={classes.inputParagraph}>Repeat Password</p>
-     <input className={classes.inputField} type="password" placeholder=""/>
-     <button className={classes.loginButton}>Login</button>
-    </div>
-</div> 
+
+const switchToChoiceDiv = () => {
+  this.setState({
+    class: " animated fadeOutUp"
+  })
+  setTimeout(() => {
+    this.setState({
+      displayedContent:
+      <div className={classes.choiceDiv}>
+        <h1 className={classes.choiceHeader + " animated fadeInUp"}>Welcome to Workout Planner</h1>
+        <button className={classes.choiceButton + " animated fadeInUp"}>Login</button>
+        <p className={classes.choiceText + " animated fadeInUp"}>or</p>
+        <button className={classes.choiceButton + " animated fadeInUp"}>Sign Up</button>
+      </div> 
+    })
+  }, 1000);
+}
+
+if (this.state.displayedContent) {
+  return (
+   <div className={classes.mainDiv}>
+     {this.state.displayedContent}
+   </div>
   )
+} else {
+  return (
+    <div className={classes.mainDiv}>
+      <div className={classes.contentDiv + this.state.class}>
+        <h1 className={classes.mainHeader}>Workout Planner</h1>
+        <h4 className={classes.describtionHeader}>Add your own workout, check your progress and use the timers between sets</h4>
+        <button onClick={switchToChoiceDiv} className={classes.getStartedButton}>Get Started</button>
+      </div>
+    </div>
+    )
+  }
  }
 }
 
