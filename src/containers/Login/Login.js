25f6fa0f93d02c06038b40886970ecb9a2ee9f1b
Kacper <kacper.biedka.00@gmail.com>
2019-04-28 01:42:19 +0200

    Made the new login design responsive in 2k and 4k

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -21,99 +21,101 @@ class Login extends Component {
     });
   };
 
+  loginWithTwitter = () => {
+    const provider = new firebase.auth.TwitterAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(function(result) {
+        var token = result.credential.accessToken;
+        var user = result.user;
+        console.log("login info: ", token, user);
+        window.location.href = "/workouts";
+      })
+      .catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        alert("There is no twitter user signed in");
+      });
+  };
+
+  updateEmailState = e => {
+    this.setState({
+      email: e.target.value
+    });
+  };
+
+  updatePasswordState = e => {
+    this.setState({
+      password: e.target.value
+    });
+  };
+
+  loginWithEmailAndPassword = () => {
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(this.state.email, this.state.password)
+      .then(function() {
+        window.location.href = "/workouts";
+      })
+      .catch(error => {
+        var errorComponent = null;
+        var errorMessage = error.message;
+        console.log(errorMessage);
+        errorComponent = (
+          <div className={classes.loginErrorDiv}>{error.message}</div>
+        );
+        this.setState({
+          loginError: errorComponent
+        });
+      });
+  };
+
+  loginWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(function(result) {
+        var token = result.credential.accessToken;
+        var user = result.user;
+        console.log("login info: ", token, user);
+        window.location.href = "/workouts";
+      })
+      .catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        alert("Error logging in: " + error.message);
+      });
+  };
+
+  loginWithFacebook = () => {
+    const provider = new firebase.auth.FacebookAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(function(result) {
+        var token = result.credential.accessToken;
+        var user = result.user;
+        console.log("login info: ", token, user);
+        window.location.href = "/workouts";
+      })
+      .catch(function(error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        alert("Error logging in: " + error.message);
+      });
+  };
+
+  redirectToSignUp = () => {};
+
   render() {
-    const updateEmailState = e => {
-      this.setState({
-        email: e.target.value
-      });
-    };
-
-    const updatePasswordState = e => {
-      this.setState({
-        password: e.target.value
-      });
-    };
-
-    const loginWithEmailAndPassword = () => {
-      firebase
-        .auth()
-        .signInWithEmailAndPassword(this.state.email, this.state.password)
-        .then(function() {
-          window.location.href = "/workouts";
-        })
-        .catch(error => {
-          var errorComponent = null;
-          var errorMessage = error.message;
-          console.log(errorMessage);
-          errorComponent = (
-            <div className={classes.loginErrorDiv}>{error.message}</div>
-          );
-          this.setState({
-            loginError: errorComponent
-          });
-        });
-    };
-
-    const loginWithGoogle = () => {
-      const provider = new firebase.auth.GoogleAuthProvider();
-      firebase
-        .auth()
-        .signInWithPopup(provider)
-        .then(function(result) {
-          var token = result.credential.accessToken;
-          var user = result.user;
-          console.log("login info: ", token, user);
-          window.location.href = "/workouts";
-        })
-        .catch(function(error) {
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          var email = error.email;
-          var credential = error.credential;
-          alert("Error logging in: " + error.message);
-        });
-    };
-
-    const loginWithFacebook = () => {
-      const provider = new firebase.auth.FacebookAuthProvider();
-      firebase
-        .auth()
-        .signInWithPopup(provider)
-        .then(function(result) {
-          var token = result.credential.accessToken;
-          var user = result.user;
-          console.log("login info: ", token, user);
-          window.location.href = "/workouts";
-        })
-        .catch(function(error) {
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          var email = error.email;
-          var credential = error.credential;
-          alert("Error logging in: " + error.message);
-        });
-    };
-
-    const loginWithTwitter = () => {
-      const provider = new firebase.auth.TwitterAuthProvider();
-      firebase
-        .auth()
-        .signInWithPopup(provider)
-        .then(function(result) {
-          var token = result.credential.accessToken;
-          var user = result.user;
-          console.log("login info: ", token, user);
-          window.location.href = "/workouts";
-        })
-        .catch(function(error) {
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          var email = error.email;
-          var credential = error.credential;
-          alert("There is no twitter user signed in");
-        });
-    };
-
     return (
       <div className={classes.mainLoginDiv}>
         <div className={classes.authDiv + " animated zoomIn"}>
@@ -121,25 +123,44 @@ class Login extends Component {
           <div className={classes.standardLoginDiv}>
             <input
               className={classes.inputField}
-              onChange={updateEmailState}
+              onChange={this.updateEmailState}
               type="email"
               placeholder="Email"
             />
             <input
               className={classes.inputField + " " + classes.passwordInputField}
-              onChange={updatePasswordState}
+              onChange={this.updatePasswordState}
               type="password"
               placeholder="Password"
             />
             <button
               className={classes.loginButton + " " + classes.buttonAnimation}
-              onClick={loginWithEmailAndPassword}
+              onClick={this.loginWithEmailAndPassword}
             >
               <span className={classes.loginSpan}>Sign In</span>
             </button>
             <div className={classes.forgotPasswordDiv}>
               <p className={classes.forgotPassword}>Forgot password ?</p>
             </div>
+          </div>
+          <div className={classes.differentOptionsDiv}>
+            <p className={classes.differentOptionsHeader}>
+              Different login options
+            </p>
+            <i
+              onClick={this.loginWithGoogle}
+              className={"fab fa-google " + classes.googleIcon}
+            />
+            <i
+              onClick={this.loginWithFacebook}
+              className={"fab fa-facebook " + classes.facebookIcon}
+            />
+          </div>
+          <div className={classes.signUpDiv}>
+            <p onClick={this.redirectToSignUp} className={classes.signUpHeader}>
+              Don't have an account ?
+            </p>
+            <p className={classes.signUpAnchor}>Sign Up</p>
           </div>
         </div>
       </div>
